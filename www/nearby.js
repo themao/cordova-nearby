69369f4ecc216f42d4bb3181df0e7dd9da72d08c
Anton Martynenko <cn4747@gmail.com>
2017-03-27 17:54:17 +0300

    Added version, package Added gitignore, removed idea files

--- www/nearby.js
+++ www/nearby.js
@@ -1,5 +1,7 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "nearby", "coolMethod", [arg0]);
+//exports.start = function(arg0, success, error) {
+exports.start = function(success, error) {
+//    exec(success, error, "nearby", "start", [arg0]);
+    exec(success, error, "nearby", "start");
 };
