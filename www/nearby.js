e073cebfbc1ac175db84bcd842520f8a2f01857a
Anton Martynenko <cn4747@gmail.com>
2017-03-27 18:23:45 +0300

    Changed exec call

--- www/nearby.js
+++ www/nearby.js
@@ -1,7 +1,5 @@
-var exec = require('cordova/exec');
-
 //exports.start = function(arg0, success, error) {
 exports.start = function(success, error) {
 //    exec(success, error, "nearby", "start", [arg0]);
-    exec(success, error, "nearby", "start");
+    cordova.exec(success, error, "nearby", "start");
 };
