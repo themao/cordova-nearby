4b3a07d51447d0c4199e67cd7a5c22f1547be786
Anton Martynenko <cn4747@gmail.com>
2017-03-27 18:28:55 +0300

    Changed exports

--- www/nearby.js
+++ www/nearby.js
@@ -1,5 +1,11 @@
 //exports.start = function(arg0, success, error) {
-exports.start = function(success, error) {
+//exports.start = function(success, error) {
 //    exec(success, error, "nearby", "start", [arg0]);
-    cordova.exec(success, error, "nearby", "start");
+//    cordova.exec(success, error, "nearby", "start");
+//};
+
+module.exports = {
+    start: function (name, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "nearby", "start");
+    }
 };
